638ca78a2a50dff7abebe2b9d6afee60031f00c9
nalse <54538144+nalse@users.noreply.github.com>
2024-02-18 01:58:40 +0400

    Update app.js
    domain change

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const api = require("./routes/api")
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://server-musicapp.ondigitalocean.app"
 }))
 app.use(express.json());
 

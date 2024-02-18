d28fc7c9a6e9117136fc87366fea649b30ca04fc
nalse <54538144+nalse@users.noreply.github.com>
2024-02-18 19:13:40 +0400

    Update server.js
    port update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const mongoose = require("mongoose")
 
-const PORT = 5000;
+const PORT = 80;
 const DB = "mongodb+srv://ulka:ulka2005@cluster0.0ztiq13.mongodb.net/test3"
 
 const app = require("./app");
